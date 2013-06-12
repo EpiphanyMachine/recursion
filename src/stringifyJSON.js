a61feab8599e6e6a96d76a1ee2c1e7c65919d0b3
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-12 16:08:09 -0700

    Added support for null, numbers and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,9 +3,60 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
+	var string;
+	if ( Object.prototype.toString.call( obj ) === '[object Number]' ) {
+		string = obj + ',';
+	}
+	if ( Object.prototype.toString.call( obj ) === '[object String]' ) {
+		string = '"' + obj + '",';
+	}
+	if ( Object.prototype.toString.call( obj ) === '[object Array]' ) {
+		string = stringifyJSONarray(obj);
+	}
+	if ( Object.prototype.toString.call( obj ) === '[object Object]' ) {
+		string = stringifyJSONobj(obj);
+		string = string.substring(0, string.length - 1) + '},';
+	}
+	return string.substring(0, string.length - 1);
+};
+
+var stringifyJSONobj = function (obj) {
 	var string = '{';
 	for (var key in obj) {
-		string += '"' + key + '":"' + obj[key] + '",';
+		string += '"' + key + '":';
+		if ( Object.prototype.toString.call( obj[key] ) === '[object Number]' ) {
+			string += obj[key] + ',';
+		}
+		if ( Object.prototype.toString.call( obj[key] ) === '[object String]' ) {
+			string += '"' + obj[key] + '",';
+		}
+		if ( Object.prototype.toString.call( obj[key] ) === '[object Array]' ) {
+			string += stringifyJSONarray(obj[key]);
+		}
+		if ( Object.prototype.toString.call( obj[key] ) === '[object Object]' ) {
+			string += stringifyJSONobj(obj[key]);
+			string = string.substring(0, string.length - 1) + '},';
+		}
 	}
 	return string.substring(0, string.length - 1) + '}';
+};
+
+var stringifyJSONarray = function (obj) {
+	var string = '[';
+	for (var i = 0; i < obj.length; i++) {
+		if ( Object.prototype.toString.call( obj[i] ) === '[object Number]' ) {
+			string += obj[i] + ',';
+		}
+		if ( Object.prototype.toString.call( obj[i] ) === '[object String]' ) {
+			string += '"' + obj[i] + '",';
+		}
+		if ( Object.prototype.toString.call( obj[i] ) === '[object Array]' ) {
+			string += stringifyJSONarray(obj[i]);
+		}
+		if ( Object.prototype.toString.call( obj[i] ) === '[object Object]' ) {
+			string += stringifyJSONobj(obj[i]);
+			string = string.substring(0, string.length - 1) + '},';
+		}
+	}
+	return string.substring(0, string.length - 1) + '],';
 };
